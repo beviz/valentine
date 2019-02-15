bb4f0a06a2a136403974188bd65757afe26d90b9
bevis <i@bevis.me>
2019-02-15 17:15:39 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+var moment = require("moment");
+var axios = require("axios");
+
+window.onload = function() {
+  var when = "2017-02-14T13:57:07.631Z";
+  var computeTime = function() {
+    var formated = "YYYY-MM-DD HH:mm:ss";
+
+    var startTime = moment(new Date(when)).format(formated);
+
+    var now = moment().format(formated);
+
+    var ms = moment(now).diff(moment(startTime));
+
+    var duration = moment.duration(ms);
+    // var y = duration.years();
+    var d = Math.floor(duration.asDays());
+    var h = duration.hours();
+    var m = duration.minutes();
+    var s = duration.seconds();
+    // document.querySelector(".year").innerText = y;
+    document.querySelector(".day").innerText = d;
+    document.querySelector(".hour").innerText = h;
+    document.querySelector(".minute").innerText = m;
+    document.querySelector(".second").innerText = s;
+    requestAnimationFrame(computeTime);
+  };
+
+  var catchError = function() {
+    document.querySelector(".prompt").style.display = "block";
+  };
+
+  var onSuccess = function(res) {
+    when = res.data.when;
+    document.querySelector(".prompt").style.display = "none";
+    document.querySelector(".content").style.display = "block";
+    requestAnimationFrame(computeTime);
+    var manifesto = document.querySelector(".manifesto");
+    manifesto.style.display = "block";
+    manifesto.innerText = res.data.manifesto;
+  };
+
+  // axios
+  //   .get("/ido/xx.json")
+  //   .then(function(res) {
+  //     if (!res || res.status < 200 || res.status >= 300 || !res.data) {
+  //       throw "error";
+  //     }
+  //     onSuccess(res);
+  //   })
+  //   .catch(catchError);
+
+  onSuccess({ data: { when, manifesto: "这是一条宣言" } });
+};
